b0fab09ac60e4a2dd3a743f284e9ea6cc225852e
lukesalamone <lukesalamone94@gmail.com>
2021-02-13 22:53:58 -0600

    add hyperparams to main.py

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
-#Batches the data with [T, B] dimensionality.
-def minibatch(data, batch_size, seq_length):
-    data = torch.tensor(data, dtype = torch.int64)
-    num_batches = data.size(0)//batch_size
-    data = data[:num_batches*batch_size]
-    data=data.view(batch_size,-1)
-    dataset = []
-    for i in range(0,data.size(1)-1,seq_length):
-        seqlen=int(np.min([seq_length,data.size(1)-1-i]))
-        if seqlen<data.size(1)-1-i:
-            x=data[:,i:i+seqlen].transpose(1, 0)
-            y=data[:,i+1:i+seqlen+1].transpose(1, 0)
-            dataset.append((x, y))
-    return dataset
+from lstm import Model
+
+
+# (i) embedding space dimensionality,
+# (ii) vocabulary frequency threshold,
+# (iii) a keep probability for dropout,
+# (iv) an initialization range,
+# (v) number of training epochs,
+# (vi) an initial learning rate and decay schedule,
+# (vii) number of layers,
+# (viii) batch size,
+# (ix) step size,
+# (x) maximum gradient,
+# (xi) embedding tying,
+# (xii) a bias flag, and any others that you think may be necessary.
+
+def main():
+    hyperparams = {
+        'embed_dims': None, #
+        'freq_threshold': 3,
+        'dropout_prob': 0.5, #
+        'init_range': 0.05, #
+        'epochs': 40,
+        'learning_rate': 1,
+        'learning_rate_decay': 1.2,
+        'num_layers': 2, #
+        'batch_size': 20,
+        'step_size': 35,
+        'max_grad': 5,          # ???
+        'embed_tying': False,   # ???
+        'bias': False           # ???
+    }
+
+
+
+    train_data, valid_data, test_data = get_data(threshold=hyperparams['freq_threshold'])
+
+    model_params = ['embed_dims', 'dropout_prob', 'init_range', 'num_layers', 'step_size', 'max_grad', 'embed_tying', 'bias']
+    model_params = {k:hyperparams[k] for k in model_params}
+    model = Model(**model_params)
+
+
+
+def get_data():
+    return None, None, None
